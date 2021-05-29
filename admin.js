9a22e6fc9c90f8ea4dfa89b099da321f6a093121
Majid Alessio <majidalessio@gmail.com>
2021-05-29 15:48:22 +0200

    non funziona ma dovrebbe essere giusto almeno al 90%

--- admin.js
+++ admin.js
@@ -1,112 +1,97 @@
 //prendi le tipologie da php
-function getTipologie()
-{
-     caricatipologie()          //RICOORDATI DI LEVARE QUESTO QUANDO FUNZIONERà
-     let conn=new XMLHttpRequest();                        
-     conn.open("GET","/area-utente/admin.php",true);              
-     conn.onreadystatechange=function()
-     {
-         if(this.readyState==XMLHttpRequest.DONE&&this.status==200)
-         {
-             localStorage.setItem("tipologie", conn.response);
-             caricatipologie() 
-         }
-     };
-     conn.send();            
+function getTipologie() {
+    caricatipologie()          //RICOORDATI DI LEVARE QUESTO QUANDO FUNZIONERà
+    // let conn = new XMLHttpRequest();
+    // conn.open("GET", "/area-utente/admin.php", true);
+    // conn.onreadystatechange = function () {
+    //     if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+    //         localStorage.setItem("tipologie", conn.response);
+    //         caricatipologie()
+    //     }
+    // };
+    // conn.send();
 }
 
 //stampi le tipologie
-function creaTipologie()
-{
-   let tipologie= localStorage.getItem("tipologie");
-   for(let i=0;i<tipologie.length;i++)
-   {
-        let op =document.createElement("p");
+function creaTipologie() {
+    let tipologie = localStorage.getItem("tipologie");
+    for (let i = 0; i < tipologie.length; i++) {
+        let op = document.createElement("p");
         op.setAttribute("id", "opzione");
-        op.innerHTML=tipologie[i]["tipologia"] + tipologie[i]["difficolta"] + " " + tipologie[i]["nome"]+ " "  + tipologie[i]["cognome"];
+        op.innerHTML = tipologie[i]["tipologia"] + tipologie[i]["difficolta"] + " " + tipologie[i]["nome"] + " " + tipologie[i]["cognome"];
         let div = document.getElementById("selectTipologie");
         div.appendChild(op);
-   }
+    }
 }
 
 //form iniziale onload alla pagina
-function caricatipologie()
-{
-    let html= '<div id="wrapper"><div name="tipologie" id="selectTipologie"> </div></div>'+
-              '<div id="button"><input type="button" id="bottoneCambio" onclick="cambiaTipologia()" value="Inserisci corso"></button></div>';
+function caricatipologie() {
+    let html = '<div id="wrapper"><div name="tipologie" id="selectTipologie"> </div></div>' +
+        '<div id="button"><input type="button" id="bottoneCambio" onclick="cambiaTipologia()" value="Inserisci corso"></button></div>';
     //  creaTipologie();        RICORDATI DI SCOMMENTARLO
-     document.getElementById("divTipologie").innerHTML= html;
+    document.getElementById("divTipologie").innerHTML = html;
 }
 
-function inserisciCorso()
-{
-    let html='<div id="wrapper"><input type="text" id="input" placeholder="inserisci tipologia..."></div>'+
-             '<div id="wrapper"><input type="text" id="input" placeholder="inserisci nome tutor..."></div>'+
-             '<div id="wrapper"><input type="text" id="input" placeholder="inserisci cognome tutor..."></div>'+
-             '<div id="button"><input type="button" id="bottoneCambio" onclick="cambiaTipologia()" value="Inserisci corso"></button></div>'+
-             '<div id="submitbutton"><input type="button" id="bottoneInvio" value="Aggiungi" onclick="sendData()"></button>'
-    document.getElementById("divTipologie").innerHTML= html;
+function inserisciCorso() {
+    let html = '<div id="wrapper"><input type="text" class="inputText" id="tipo" placeholder="inserisci tipologia..."></div>' +
+        '<div id="wrapper"><input type="text" class="inputText" id="nome" placeholder="inserisci nome tutor..."></div>' +
+        '<div id="wrapper"><input type="text" class="inputText" id="cognome" placeholder="inserisci cognome tutor..."></div>' +
+        '<div id="button"><input type="button" id="bottoneCambio" onclick="cambiaTipologia()" value="Inserisci corso"></button></div>' +
+        '<div id="submitbutton"><input type="button" id="bottoneInvio" value="Aggiungi" onclick="sendData()"></button>'
+    document.getElementById("divTipologie").innerHTML = html;
 }
 
 //quello che fa il bottone
-function cambiaTipologia()
-{
-   if(document.getElementById("bottoneCambio").value =="Inserisci corso")
-   {
-        let num= document.querySelectorAll("[id='wrapper']");
+function cambiaTipologia() {
+    if (document.getElementById("bottoneCambio").value == "Inserisci corso") {
+        let num = document.querySelectorAll("[id='wrapper']");
         //cancella la roba che vedi appena carichi
-        for(let i=0;i<num.length;i++)
-        {
-            num[i].style.visibility ="hidden";
+        for (let i = 0; i < num.length; i++) {
+            num[i].style.visibility = "hidden";
         }
         inserisciCorso();
-        document.getElementById("bottoneCambio").value ="Vedi corsi";
-   }
-   else //riporti a quando carichi la pagina
-   {
-        let num= document.querySelectorAll("[id='wrapper']");
+        document.getElementById("bottoneCambio").value = "Vedi corsi";
+    }
+    else //riporti a quando carichi la pagina
+    {
+        let num = document.querySelectorAll("[id='wrapper']");
         //ristampa la roba che vedi appena carichi
-        for(let i=0;i<num.length;i++)
-        {
-            num[i].style.visibility ="visible";
+        for (let i = 0; i < num.length; i++) {
+            num[i].style.visibility = "visible";
         }
         caricatipologie();
-        document.getElementById("bottoneCambio").value ="Inserisci corso";
-   }
+        document.getElementById("bottoneCambio").value = "Inserisci corso";
+    }
 }
 
 //API
-function sendData()
-{
-    $(document).on('click','#bottoneInvio',function()
-    {
-        let valori= Array();
-        for(let i=0;i<3;i++)
-        {
-             valori[i] =$("#input")[i].value;
-        }
+function sendData() {
+    $(document).on('click', '#bottoneInvio', function () {
+        let tipo = $("#tipo").val();
+        let nome= $("#nome").val();
+        let cognome = $("#cognome").val();
+        console.log(tipo);
         //creation of the JSON
-        let postData = 
+        let postData =
         {
-            "tipologia": valori[0],
-            "nome" : valori[1],
-            "cognome" : valori[2],
-        } ;
+            "tipologia": tipo,
+            "nome": nome,
+            "cognome": cognome,
+        };
         //asynchronous http request
         $.ajax({
-        url: "/area-utente/admin.php",
-        type: "post",
-        contentType: "application/json",
-        data: JSON.stringify({"tipologie": postData}),
-        accept: "*/*",
-        success: function(data)
-        {  
-            getTipologie();
-        },
-        error: function(errorThrown)
-        {
-            console.log( errorThrown );
-        }
+            url: "area-utente/admin.php",
+            type: "post",
+            contentType: "application/json",
+            data: JSON.stringify({ "tipologie": postData }),
+            accept: "*/*",
+            success: function (data) {
+                getTipologie();     //se funziona esegue la funzione per prendere le tipologie così si aggiorna quando torni indietro
+                cambiaTipologia(); //torna alla pagina prima
+            },
+            error: function (errorThrown) {
+                console.log(errorThrown);
+            }
         });
     });
 }
--- admin.php
+++ admin.php
@@ -1,29 +1,59 @@
 <?php
 
+// questa ti prende le tipologie dal db, commentatela come ti pare
 function getTipologie()
 {
-    if(empty($errors))
-	{
+    if (empty($errors)) {
         include 'config.php';
         $conn = mysqli_connect($servername, $username, $password,  $dbname);
         if (!$conn) {
-			die("Connection failed: " . mysqli_connect_error());
-		}
+            die("Connection failed: " . mysqli_connect_error());
+        }
 
         $sql = "SELECT tipologia, difficolta, nome, cognome 
         FROM corso inner join assegnazione on idCorso=	id_corso inner join educatori on idEducatore=id_educatore";
-		$result = mysqli_query($conn, $sql);
-        if(mysqli_num_rows($result) > 0)
-        {
-            while($row = mysqli_fetch_assoc($result))
-            {
+        $result = mysqli_query($conn, $sql);
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
                 echo json_encode($row);
             }
-        }
-        else{
+        } else {
             die();
         }
     }
 }
 
-?>
+//funzione per inserire un nuovo corso
+function insertNuovoCorso()
+{
+    include 'config.php';
+    $conn = mysqli_connect($servername, $username, $password,  $dbname);
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    $data = file_get_contents("php://input");       //prende dall'api
+    $data = json_decode($data, true);               //array associativo
+    insertEducatore();
+    //inserisce in corso
+    $stmt = $conn->prepare("INSERT INTO corso (tipologia) VALUES (?)");
+    $stmt->bind_param("s", $tipologia);
+    $tipologia=$data["tipologia"];
+    $stmt->execute();
+}
+
+//funzione per inserire un educatore
+function insertEducatore()
+{
+    include 'config.php';
+    $conn = mysqli_connect($servername, $username, $password,  $dbname);
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    $data = file_get_contents("php://input");       //prende dall'api
+    $data = json_decode($data, true);               //array associativo
+    $stmt = $conn->prepare("INSERT INTO educatori (cognome,nome) VALUES (?,?)");
+    $stmt->bind_param("ss", $nome, $cognome);
+    $nome = $data["nome"];
+    $cognome= $data["cognome"];
+    $stmt->execute();
+}
